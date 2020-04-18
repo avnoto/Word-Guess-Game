e87a6419898dda61d57872b0b2d00c3ab5831939
avnoto <alexandravnoto@gmail.com>
2020-04-18 01:38:18 -0700

    added instructions and buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,43 +3,37 @@ window.onload = function() {
   }
   
 function fadeout() {
-    document.getElementById('fadeout').style.opacity = '0'; 
+    document.getElementById("fadeout").style.opacity = "0"; 
   }
 
 
 function intro() {
     fadeout();
+    window.setTimeout(logoFadeIn, 1000);
+    logoFadeIn();
     window.setTimeout(fadein, 1000);
+}
+function logoFadeIn() {
+    document.getElementById("got-logo").style.opacity = "100";
 }
 
 function fadein(){
-    document.getElementById('welcome-container').style.opacity = '100';
+    document.getElementById("welcomeText").style.opacity = "100";
 
 }
 
-
-
-
-
-
-
-
-
-let playButton = document.getElementById('play-btn');
-playButton.addEventListener('click', startGame);
-
-function startGame() {
-playButton.classList.add('hide');
-
-}
 
 function instructions() {
-
-
-}
+    document.getElementById("welcomePage").style.display = "none";
+    document.getElementById("instructions-container").style.display = "block";
+    //display: none for game page
+    }
 
 function backToMain() {
-
-
-
+    document.getElementById("instructions-container").style.display = "none";
+    document.getElementById("welcomePage").style.display = "block";
+    //display: none for game  page
 }
+
+//need function for startGame
+
