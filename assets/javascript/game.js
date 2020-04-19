9ef48208318d023d9a7eace967d671ece8639cc3
avnoto <alexandravnoto@gmail.com>
2020-04-18 23:58:24 -0700

    dynamically generated letter divs and refactor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    createLetters();
     window.setTimeout(intro, 3000);
   }
   
@@ -22,7 +23,6 @@ function fadein(){
 
 }
 
-
 function instructions() {
     document.getElementById("welcomePage").style.display = "none";
     document.getElementById("instructions-container").style.display = "block";
@@ -40,34 +40,20 @@ function startGame() {
     document.getElementById("instructions-container").style.display = "none";
     document.getElementById("welcomePage").style.display = "none";
     document.getElementById("gamePage").style.display = "block";
-    document.getElementById("challengeWord").style.display = "block";
     document.getElementById("maxWrong").innerHTML = maxWrong;
 
-    function guessedWord() {
-        wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " __ ")).join("");
-        document.getElementById("wordSpotlight").innerHTML = wordStatus;
-    }
-    
+    document.addEventListener('keydown', function(event) { 
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            userGuess(String.fromCharCode(event.keyCode));
+        }
+    });
+            
     randomWord();
     guessedWord();
-
-    function userGuess(chosenLetter) {
-        guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter): null;
-        document.getElementById(chosenLetter).setAttribute("disabled", true);
-    
-        if (answer.indexOf(chosenLetter) >= 0) {
-            guessedWord();
-        } else if (answer.indexOf(chosenLetter) === -1) {
-            mistakes++;
-            updateMistakes();
-        }
-    }
-
-    userGuess();
 }
 
 function randomWord() {
-    answer = wordBank[Math.floor(Math.random() * wordBank.length)];
+    answer = wordBank[Math.floor(Math.random() * wordBank.length)].toLowerCase();
 }
 
 function updateMistakes() {
@@ -80,29 +66,58 @@ var mistakes = 0;
 var guessed = [];
 var wordStatus = null;
 
-var wordBank = ["Baratheon", "Stark", "Targaryen"];
-var firstWord = ["Baratheon"];
-var secondWord = ["Stark"];
-var thirdWord = ["Targaryen"];
+var wordBank = ["baratheon", "stark", "targaryen"];
+//var firstWord = ["Baratheon"];
+//var secondWord = ["Stark"];
+//var thirdWord = ["Targaryen"];
 
-var x = firstWord.length;
-var y = secondWord.length;
-var z = thirdWord.length;
-
-
+//var x = firstWord.length;
+//var y = secondWord.length;
+//var z = thirdWord.length;
 
 
-function userGuess() {
-    var target = event.target || event.srcElement;
-    target.style.visibility = "hidden";
-    var letter = document.getElementById(letter).getAttribute("value");
-    
+function guessedWord() {
+    wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " __ ")).join("");
+    document.getElementById("wordSpotlight").innerHTML = wordStatus;
 }
 
+function userGuess(rawChosenLetter) {
+    if (!rawChosenLetter) return;
+    let chosenLetter = rawChosenLetter.toLowerCase();
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter): null;
 
+    if (answer.indexOf(chosenLetter) >= 0) {
+        guessedWord();
+        checkIfGameWon();
+    } else if (answer.indexOf(chosenLetter) === -1) {
+        mistakes++;
+        updateMistakes();
+        checkIfGameLost();
+    }
+}
 
+function checkIfGameWon() {
+    if (wordStatus === answer) {
+        document.getElementById("win-lose").innerHTML = "You Win!";
+    }
+}
 
+function checkIfGameLost() {
+    if (mistakes === maxWrong) {
+        document.getElementById("win-lose").innerHTML = "You Lost!";
+    }
+}
 
+function createLetters() {  
+    for (i = 0; i < 26; i++) {    
+        let letter = (i + 10).toString(36);    
+        let letterBank = document.getElementById('letterBank')    
+        let newDiv = document.createElement("div");    
+        newDiv.innerHTML = letter.toUpperCase();    
+        newDiv.addEventListener("click", () => { userGuess(letter) }, false);
+        letterBank.appendChild(newDiv);
 
+    }
+}
 
 
