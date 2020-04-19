c1af735b59b9e1b5cebccaf39a96bf45609e6d7e
avnoto <alexandravnoto@gmail.com>
2020-04-19 01:54:20 -0700

    fully working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,21 @@
+var answer = "";
+const maxWrong = 5;
+var mistakes = 0;
+var guessed = [];
+var wordStatus = null;
+var gameOver = false;
+
+var wordBank = ["baratheon", "stark", "targaryen", "iron throne"];
+
+
+
 window.onload = function() {
+    document.addEventListener('keydown', function(event) { 
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            let letter = String.fromCharCode(event.keyCode).toLowerCase();
+            if (guessed.indexOf(letter) === -1) userGuess(letter);
+        }
+    });
     createLetters();
     window.setTimeout(intro, 3000);
   }
@@ -37,19 +54,21 @@ function backToMain() {
 }
 
 function startGame() {
+    showLetters();
+    mistakes = 0;
+    guessed = [];
+    wordStatus = null;
+    gameOver = false;
+    updateMistakes();
+    document.getElementById("winLose").style.display = "none";
     document.getElementById("instructions-container").style.display = "none";
     document.getElementById("welcomePage").style.display = "none";
     document.getElementById("gamePage").style.display = "block";
     document.getElementById("maxWrong").innerHTML = maxWrong;
-
-    document.addEventListener('keydown', function(event) { 
-        if (event.keyCode >= 65 && event.keyCode <= 90) {
-            userGuess(String.fromCharCode(event.keyCode));
-        }
-    });
             
     randomWord();
     guessedWord();
+
 }
 
 function randomWord() {
@@ -60,32 +79,18 @@ function updateMistakes() {
     document.getElementById("mistakes").innerHTML = mistakes;
 }
 
-var answer = "";
-var maxWrong = 5;
-var mistakes = 0;
-var guessed = [];
-var wordStatus = null;
-
-var wordBank = ["baratheon", "stark", "targaryen"];
-//var firstWord = ["Baratheon"];
-//var secondWord = ["Stark"];
-//var thirdWord = ["Targaryen"];
-
-//var x = firstWord.length;
-//var y = secondWord.length;
-//var z = thirdWord.length;
-
 
 function guessedWord() {
-    wordStatus = answer.split("").map(letter => (guessed.indexOf(letter) >= 0 ? letter : " __ ")).join("");
+    wordStatus = answer.split("").map(letter => letter == " " ? "&nbsp;" : (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join("");
     document.getElementById("wordSpotlight").innerHTML = wordStatus;
 }
 
 function userGuess(rawChosenLetter) {
+    if (gameOver) return;
     if (!rawChosenLetter) return;
     let chosenLetter = rawChosenLetter.toLowerCase();
     guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter): null;
-
+    document.getElementById(chosenLetter).style.visibility = "hidden";
     if (answer.indexOf(chosenLetter) >= 0) {
         guessedWord();
         checkIfGameWon();
@@ -98,13 +103,19 @@ function userGuess(rawChosenLetter) {
 
 function checkIfGameWon() {
     if (wordStatus === answer) {
-        document.getElementById("win-lose").innerHTML = "You Win!";
+        let winLose = document.getElementById("winLose");
+        winLose.style.display = "block";
+        winLose.innerHTML = "You Win!";
+        gameOver = true;
     }
 }
 
 function checkIfGameLost() {
     if (mistakes === maxWrong) {
-        document.getElementById("win-lose").innerHTML = "You Lost!";
+        let winLose = document.getElementById("winLose");
+        winLose.style.display = "block";
+        winLose.innerHTML = "You Lost!";
+        gameOver = true;
     }
 }
 
@@ -113,10 +124,18 @@ function createLetters() {
         let letter = (i + 10).toString(36);    
         let letterBank = document.getElementById('letterBank')    
         let newDiv = document.createElement("div");    
-        newDiv.innerHTML = letter.toUpperCase();    
+        newDiv.innerHTML = letter.toUpperCase(); 
+        newDiv.id = letter;   
         newDiv.addEventListener("click", () => { userGuess(letter) }, false);
         letterBank.appendChild(newDiv);
 
+    }
+}
+
+function showLetters() {  
+    for (i = 0; i < 26; i++) {    
+        let letter = (i + 10).toString(36); 
+        document.getElementById(letter).style.visibility = "visible";
     }
 }
 
